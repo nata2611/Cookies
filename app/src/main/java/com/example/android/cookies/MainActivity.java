2cf3490bc0cf2a6ecc50b9e5dabfeee6e83b5aa7
nata2611 <natalia.v.taran@gmail.com>
2017-11-18 19:37:51 +0200

    add two buttons

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -28,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
         // TODO: Find a reference to the TextView in the layout. Change the text.
         TextView txtStatus = (TextView) findViewById(R.id.status_text_view);
         txtStatus.setText("I'm so full");
+
+        Button eatCookieButton = findViewById(R.id.eat_cookie_button);
+        Button beHungryButton = findViewById(R.id.be_hungry_button);
+        eatCookieButton.setVisibility(View.GONE);
+        beHungryButton.setVisibility(View.VISIBLE);
+
+
+    }
+
+    public void beHungry(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView imgCookies = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imgCookies.setImageResource(R.drawable.before_cookie);
+
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView txtStatus = (TextView) findViewById(R.id.status_text_view);
+        txtStatus.setText("I'm so hungry");
+
+        Button eatCookieButton = findViewById(R.id.eat_cookie_button);
+        Button beHungryButton = findViewById(R.id.be_hungry_button);
+        beHungryButton.setVisibility(View.GONE);
+        eatCookieButton.setVisibility(View.VISIBLE);
     }
 
 }
